b3f12d96106b7f901b420eeb61cfea90644523e9
salma-fullstack-developer <salmahussainweb@gmail.com>
2024-10-05 20:18:45 -0700

    done

--- Desktop/Domwhile/assest/domwhile.js
+++ Desktop/Domwhile/assest/domwhile.js
@@ -1,3 +1,18 @@
-let a=prompt("enter yuor user name");
-let b=prompt("enter your pincode");
-let pincode="0000"
+let a= prompt("Enter your username:");
+let b = "0"
+let c= "0000";
+
+do {
+    b = prompt("Enter your pincode:");
+    if (b !== c) {
+        alert("Incorrect pincode. Please try again.");
+    //    uname ++;
+    }
+} while (b!==c );
+{
+    alert('login successfuly ')
+} 
+     {
+        // username.innerHTML=a
+    document.getElementById("username").innerHTML =a
+ }
